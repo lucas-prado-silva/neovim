648f45c0d23c2d87c0a35fe4faddc093cc481c85
Lucas Silva <lucas.silva@paddypowerbetfair.com>
2022-07-10 00:57:06 +0100

    feat: add some plugins

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
-require "lucas.options"
+require("lucas.options")
+require("lucas.keymaps")
+require("lucas.plugins")
+require("lucas.colorscheme")
+require("lucas.nvim-tree-config")
+require("lucas.comment-config")
+require("lucas.telescope-config")
+require("lucas.project-config");
 
