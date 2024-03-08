8af3e1c820189cba06b970feda6e82fbe79dac4e
Lucas Silva <lucas.silva@paddypowerbetfair.com>
2024-03-08 21:35:21 +0000

    neotree

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,7 +23,8 @@ return {
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fr', builtin.resume, {})
 		vim.keymap.set('n', '<leader>fgf', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>fs', function () 
+		vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>fw', function () 
 			builtin.grep_string({ search = vim.fn.input("Grep > ")});
 		end, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
